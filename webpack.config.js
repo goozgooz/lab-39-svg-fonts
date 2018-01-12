4551fb9887ccfab8339d8ee4d953305b7ad8d6fb
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-01-12 18:20:09 +0000

    fonts added

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,102 @@
 'use strict';
 
 require('dotenv').config();
+
+// Dynamic Script and Style Tags
 const HTMLPlugin = require('html-webpack-plugin');
+
+// Makes a separate CSS bundle
 const ExtractPlugin = require('extract-text-webpack-plugin');
+
 const {EnvironmentPlugin, DefinePlugin} = require('webpack');
 
+let production = process.env.NODE_ENV === "production";
+
 let plugins = [
-  new HTMLPlugin(),
-  new ExtractPlugin('bundle.[hash].css'),
-  new EnvironmentPlugin(['NODE_ENV']),
-  new DefinePlugin({
-    '__API_URL__': JSON.stringify(process.env.API_URL),
-  }),
+    new HTMLPlugin(),
+    new ExtractPlugin('bundle.[hash].css'),
+    new EnvironmentPlugin(['NODE_ENV']),
+    new DefinePlugin({
+        '__AUTH_URL__': JSON.stringify(process.env.AUTH_URL),
+        '__API_URL__': JSON.stringify(process.env.API_URL),
+        '__DEBUG__': JSON.stringify(! production)
+    })
 ];
 
-const production = process.env.NODE_ENV === 'production';
-
 module.exports = {
-  plugins, 
 
-  devtool: 'source-map',
-  
-  entry: `${__dirname}/src/main.js`,
+    plugins,
 
-  devServer: {
-    historyApiFallback: true, // server index.html for 404 routes
-  },
-    
-  output: {
-    filename: 'bundle.[hash].js',
-    path: `${__dirname}/build`,
-  },
-    
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },  
-      {
-        test: /\.scss$/,
-        loader: ExtractPlugin.extract({
-          use: [
+    // Load this and everythning it cares about
+    entry: `${__dirname}/src/main.js`,
+
+    devServer: {
+        historyApiFallback:true
+    },
+
+    devtool: 'source-map',
+
+    // Stick it into the "path" folder with that file name
+    output: {
+        filename: 'bundle.[hash].js',
+        path: `${__dirname}/build`
+    },
+
+    module: {
+        rules: [
             {
-              loader: 'css-loader', 
-              options: {
-                sourceMap:true,
-              },
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
             },
-            'resolve-url-loader',
             {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-                includePaths:[`${__dirname}/src/style`],
-              },
+                test: /\.scss$/,
+                loader: ExtractPlugin.extract({
+                    // These get loaded in reverse order and the output of one pipes into the other (think of a then)
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap:true
+                            }
+                        },
+                        'resolve-url-loader',
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true,
+                                includePaths:[`${__dirname}/src/style`]
+                            }
+                        }
+                    ]
+                })
             },
-          ],
-        }),
-      },
-            
-    ],
-  },
-    
-};
+            {
+                test: /\.(woff|woff2|ttf|eot|glyph|\.svg)$/,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    options: {
+                      limit: 10000,
+                      name: 'font/[name].[ext]'
+                    }
+                  }
+                ]
+            },
+            {
+                test: /\.(jpg|jpeg|gif|png|tiff|svg)$/,
+                exclude: /\.glyph.svg/,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    options: {
+                      limit: 6000,
+                      name: 'image/[name].[ext]'
+                    }
+                  }
+                ]
+            },
+        ]
+    }
+
+}

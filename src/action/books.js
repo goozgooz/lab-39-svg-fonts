2cfb067d0b8f8acb5869373ca493896d15ae186e
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-01-11 16:16:09 -0800

    lab 39 setup

--- src/action/books.js
+++ src/action/books.js
@@ -2,12 +2,22 @@ import uuid from 'uuid/v4';
 import superagent from 'superagent';
 let API = `${__API_URL__}/api/books`;
 
-export const create = payload => store => {
-  superagent.post(API)
-    .send(payload)
-    .then(res => store.dispatch(createBook(res.body)))
-    .catch(console.error)
-};
+// export const create = payload => store => {
+//   superagent.post(API)
+//     .send(payload)
+//     .then(res => store.dispatch(createBook(res.body)))
+//     .catch(console.error)
+// };
+
+export const create = ({title, author}) => ({
+  type: 'BOOK_CREATE',
+  payload: {
+    title,
+    author,
+    read: false, 
+    id: uuid(),
+  },
+});
 
 export const destroy = (payload) => ({
   type: 'BOOK_DELETE',
@@ -20,13 +30,13 @@ export const read = (payload) => ({
 })
 
 
-const createBook = ({title, author}) => ({
-  type: 'BOOK_CREATE',
-  payload: {
-    title,
-    author,
-    read: false, 
-    id: uuid(),
-  },
-});
+// const createBook = ({title, author}) => ({
+//   type: 'BOOK_CREATE',
+//   payload: {
+//     title,
+//     author,
+//     read: false, 
+//     id: uuid(),
+//   },
+// });
 
